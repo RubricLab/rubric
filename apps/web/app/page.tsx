db59889469b076b8f67895de77c344590700014f
tedspare <ted.spare@gmail.com>
2023-05-25 13:04:25 -0400

    Add background grid

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button, Header } from "ui";
+import BackgroundGrid from "./components/BackgroundGrid";
 
 export default function Page() {
   return (
-    <div className="h-full w-full center bg-off-white flex-col relative">
-      <div className="center flex-col space-y-4">
+    <div className="h-full w-full center bg-off-white flex-col relative overflow-y-hidden">
+      <BackgroundGrid className="absolute translate-y-10 w-full" />
+      <div className="center flex-col space-y-4 z-10">
         <Header text="Rubric" />
         <Button />
       </div>
