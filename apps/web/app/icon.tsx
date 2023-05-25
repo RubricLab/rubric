c97419019c38c7819d6fcd935527a4aeb3697d30
tedspare <ted.spare@gmail.com>
2023-05-25 15:04:44 -0400

    Add favicon and crawling instructions

---
+++ apps/web/app/icon.tsx
@@ -0,0 +1,48 @@
+import { ImageResponse } from "next/server";
+import colors from "tailwindcss/colors";
+import config from "../tailwind.config";
+
+export const runtime = "edge";
+
+export const contentType = "image/png";
+export const size = {
+  width: 32,
+  height: 32,
+};
+
+const fontImport = fetch(
+  new URL("./fonts/CalSans-SemiBold.ttf", import.meta.url)
+).then((res) => res.arrayBuffer());
+
+const offWhite = config.theme.extend.colors["off-white"] || "white";
+
+export default async function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 28,
+          background: colors["orange"]["700"] || "orange",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          color: offWhite,
+          paddingBottom: "5px",
+        }}
+      >
+        R
+      </div>
+    ),
+    {
+      ...size,
+      fonts: [
+        {
+          name: "cal-sans",
+          data: await fontImport,
+        },
+      ],
+    }
+  );
+}
