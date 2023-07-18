3aad3b60207805415a6c4ec1e18452153572bbc7
tedspare <ted.spare@gmail.com>
2023-07-18 12:03:19 -0400

    Adjust background grid

--- apps/web/app/components/BackgroundGrid.tsx
+++ apps/web/app/components/BackgroundGrid.tsx
@@ -4,7 +4,7 @@ import tailwindConfig from "../../tailwind.config";
 const lineAttributes = {
   stroke: tailwindConfig.theme.extend.colors["stone"][400] || "gray",
   strokeWidth: 1,
-  opacity: 0.8,
+  opacity: 0.4,
   fill: "none",
 };
 
@@ -27,43 +27,36 @@ function BackgroundGrid({
         {/* Vertical lines */}
         <g className="vertical">
           {/* Curving right */}
-          <path {...lineAttributes} d="M50,0 Q50,500 50,1000" />
-          <path {...lineAttributes} d="M100,0 Q125,500 100,1000" />
-          <path {...lineAttributes} d="M150,0 Q200,500 150,1000" />
-          <path {...lineAttributes} d="M200,0 Q275,500 200,1000" />
-          <path {...lineAttributes} d="M250,0 Q350,500 250,1000" />
-          <path {...lineAttributes} d="M300,0 Q425,500 300,1000" />
-          <path {...lineAttributes} d="M350,0 Q500,500 350,1000" />
-          <path {...lineAttributes} d="M400,0 Q600,500 400,1000" />
+          <path {...lineAttributes} d="M-70,0 Q80,500 -70,1000" />
+          <path {...lineAttributes} d="M80,0 Q200,500 80,1000" />
+          <path {...lineAttributes} d="M210,0 Q300,500 210,1000" />
+          <path {...lineAttributes} d="M320,0 Q380,500 320,1000" />
+          <path {...lineAttributes} d="M410,0 Q440,500 410,1000" />
+          <path {...lineAttributes} d="M470,0 Q480,500 470,1000" />
           {/* Curving left */}
-          <path {...lineAttributes} d="M600,0 Q400,500 600,1000" />
-          <path {...lineAttributes} d="M650,0 Q500,500 650,1000" />
-          <path {...lineAttributes} d="M700,0 Q575,500 700,1000" />
-          <path {...lineAttributes} d="M750,0 Q650,500 750,1000" />
-          <path {...lineAttributes} d="M800,0 Q725,500 800,1000" />
-          <path {...lineAttributes} d="M850,0 Q800,500 850,1000" />
-          <path {...lineAttributes} d="M900,0 Q875,500 900,1000" />
-          <path {...lineAttributes} d="M950,0 Q950,500 950,1000" />
+          <path {...lineAttributes} d="M530,0 Q520,500 530,1000" />
+          <path {...lineAttributes} d="M590,0 Q560,500 590,1000" />
+          <path {...lineAttributes} d="M680,0 Q620,500 680,1000" />
+          <path {...lineAttributes} d="M790,0 Q700,500 790,1000" />
+          <path {...lineAttributes} d="M920,0 Q800,500 920,1000" />
+          <path {...lineAttributes} d="M1070,0 Q920,500 1070,1000" />
         </g>
         {/* Horizontal lines */}
         <g className="horizontal">
-          <path {...lineAttributes} d="M0,50 Q500,50 1000,50" />
-          <path {...lineAttributes} d="M0,100 Q500,125 1000,100" />
-          <path {...lineAttributes} d="M0,150 Q500,200 1000,150" />
-          <path {...lineAttributes} d="M0,200 Q500,275 1000,200" />
-          <path {...lineAttributes} d="M0,250 Q500,350 1000,250" />
-          <path {...lineAttributes} d="M0,300 Q500,425 1000,300" />
-          <path {...lineAttributes} d="M0,350 Q500,500 1000,350" />
-          <path {...lineAttributes} d="M0,400 Q500,600 1000,400" />
-          {/* Curving left */}
-          <path {...lineAttributes} d="M0,600 Q500,400 1000,600" />
-          <path {...lineAttributes} d="M0,650 Q500,500 1000,650" />
-          <path {...lineAttributes} d="M0,700 Q500,575 1000,700" />
-          <path {...lineAttributes} d="M0,750 Q500,650 1000,750" />
-          <path {...lineAttributes} d="M0,800 Q500,725 1000,800" />
-          <path {...lineAttributes} d="M0,850 Q500,800 1000,850" />
-          <path {...lineAttributes} d="M0,900 Q500,875 1000,900" />
-          <path {...lineAttributes} d="M0,950 Q500,950 1000,950" />
+          {/* Curving up */}
+          <path {...lineAttributes} d="M0,-70 Q500,80 1000,-70" />
+          <path {...lineAttributes} d="M0,80 Q500,200 1000,80" />
+          <path {...lineAttributes} d="M0,210 Q500,300 1000,210" />
+          <path {...lineAttributes} d="M0,320 Q500,380 1000,320" />
+          <path {...lineAttributes} d="M0,410 Q500,440 1000,410" />
+          <path {...lineAttributes} d="M0,470 Q500,480 1000,470" />
+          {/* Curving down */}
+          <path {...lineAttributes} d="M0,530 Q500,520 1000,530" />
+          <path {...lineAttributes} d="M0,590 Q500,560 1000,590" />
+          <path {...lineAttributes} d="M0,680 Q500,620 1000,680" />
+          <path {...lineAttributes} d="M0,790 Q500,700 1000,790" />
+          <path {...lineAttributes} d="M0,920 Q500,800 1000,920" />
+          <path {...lineAttributes} d="M0,1070 Q500,920 1000,1070" />
         </g>
       </g>
     </svg>
