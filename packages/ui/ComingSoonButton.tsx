ac0a6dd2618a2d6615a64e12d698bd36bd0b05b1
tedspare <ted.spare@gmail.com>
2023-07-18 11:06:09 -0400

    Replace alert with toast

--- packages/ui/ComingSoonButton.tsx
+++ packages/ui/ComingSoonButton.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { toast } from "./ToastProvider";
+
 export const ComingSoonButton = () => {
   return (
     <button
-      className="bg-orange-600 text-white rounded-full"
-      onClick={() => alert("Coming soon")}
+      className="bg-orange-600 text-white p-4 px-6 font-light rounded-full"
+      onClick={() => toast.success("Coming soon")}
     >
       Coming soon.
     </button>
