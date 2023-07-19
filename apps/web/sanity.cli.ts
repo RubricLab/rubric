5ee112de21711ec152a69b39f32b6c5f4a79187b
Sarim Malik <sarimrasheed@gmail.com>
2023-07-19 17:31:34 -0400

    Minor change

--- apps/web/sanity.cli.ts
+++ apps/web/sanity.cli.ts
@@ -2,7 +2,7 @@ import { defineCliConfig } from "sanity/cli";
 
 export default defineCliConfig({
   api: {
-    projectId: "98ffjfuo",
-    dataset: "production",
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT,
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   },
 });
