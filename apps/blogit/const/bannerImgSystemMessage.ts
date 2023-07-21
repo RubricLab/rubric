1ef9e34d5ec253cdf4acc1f799a1148b41b33396
DexterStorey <dexter@dexterstorey.com>
2023-07-21 10:13:12 -0400

    Design system for image gen

--- apps/blogit/const/bannerImgSystemMessage.ts
+++ apps/blogit/const/bannerImgSystemMessage.ts
@@ -5,5 +5,7 @@ Make sure not to include any text in the image and make the image aesthetic and 
 
 The image must be grainy, film look, pantone and minimalist.
 
+Always use the design system: orange-500
+
 General Context: The design system of the company is low fi, high tech - think the movie her -> Blade runner with extra grain, very pantone, minimalist, understated.
 `
