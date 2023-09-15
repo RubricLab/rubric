9948f1f2b2c76377ecc796d1544c8888728cc25b
tedspare <ted.spare@gmail.com>
2023-09-15 14:24:41 -0400

    Add metadata getter

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -1,3 +1,6 @@
+import {Metadata} from 'next'
+import {DEFAULT_META, META} from './constants'
+
 // Parse string to Date object and return "Month DD, YYYY" format
 export function parseDate(input: Date | string) {
 	const date = new Date(input)
@@ -7,4 +10,34 @@ export function parseDate(input: Date | string) {
 		year: 'numeric'
 	})
 	return formattedDate
+}
+
+/**
+ * Get metadata for a page. Optionally override title, description, and preview image URL.
+ */
+export const getMetadata = ({title, description, previewImageUrl}: {title?: string; description?: string; previewImageUrl?: string}): Metadata => {
+	const combinedTitle = `${title ? `${title} | ` : ''}${META.title}`
+	return {
+		title: combinedTitle,
+		description: description || DEFAULT_META.description,
+		openGraph: {
+			...DEFAULT_META.openGraph,
+			title: combinedTitle,
+			description: description || DEFAULT_META.description,
+			...(previewImageUrl
+				? {
+						images: [
+							{
+								url: previewImageUrl
+							}
+						]
+				  }
+				: {})
+		},
+		twitter: {
+			...DEFAULT_META.twitter,
+			title: combinedTitle
+		},
+		...DEFAULT_META
+	}
 }
