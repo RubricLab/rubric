168bc0b939df99b2c3cc025fb349ebbd3a68e82b
Sarim Malik <sarim@rubriclab.com>
2023-10-13 18:50:52 -0400

    Add link to navbar logo

--- apps/web/app/components/NavBar.tsx
+++ apps/web/app/components/NavBar.tsx
@@ -1,11 +1,16 @@
 'use client'
 import {ArrowRight} from 'lucide-react'
+import Link from 'next/link'
 import Logo from './Logo'
 
 export default function NavBar() {
 	return (
 		<nav className='absolute left-[50%] top-0 z-20 flex w-full -translate-x-1/2 items-center justify-between gap-1 p-5 sm:px-10'>
-			<Logo />
+			<Link
+				href='/'
+				className='text-inherit'>
+				<Logo />
+			</Link>
 			<button className='flex w-fit items-center justify-between gap-20 rounded-md bg-orange-100 p-3 '>
 				<span className='font-neue-bit text-2xl'>Get in touch</span>
 				<ArrowRight />
