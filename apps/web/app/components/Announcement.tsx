109d06aa0a04fb360d3f0ab3d71c6499cbce2dc4
Sarim Malik <sarim@rubriclab.com>
2023-10-14 23:44:22 -0400

    Ensure keyboard shortcut is not visible on mobile

--- apps/web/app/components/Announcement.tsx
+++ apps/web/app/components/Announcement.tsx
@@ -6,7 +6,7 @@ export default function Announcement() {
 		<Link
 			className='group flex items-center gap-2 rounded-full border-2 border-neutral-400 bg-neutral-200 px-5 py-3 text-black'
 			href='https://cal.ai'>
-			<p>
+			<p className='text-sm sm:text-lg'>
 				<span className='font-semibold'>New:</span> Check out our launch with Cal.ai
 			</p>
 			<ArrowUpRight className='transition-all duration-300 group-hover:-translate-y-1 group-hover:translate-x-1' />
