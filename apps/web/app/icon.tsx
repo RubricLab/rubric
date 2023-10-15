3f64c43381a4bd28fa2a02b2730950efdafedbd4
DexterStorey <dexter@dexterstorey.com>
2023-10-15 05:20:30 -0400

    cmon icon

--- apps/web/app/icon.tsx
+++ apps/web/app/icon.tsx
@@ -4,71 +4,77 @@ export const runtime = 'edge'
 
 export const contentType = 'image/png'
 export const size = {
-	height: 30,
-	width: 30
+	height: 32,
+	width: 32
 }
 
 export default async function Icon() {
 	return new ImageResponse(
 		(
-			<svg
-				width='30'
-				height='30'
-				viewBox='0 0 30 30'
-				fill='none'
-				xmlns='http://www.w3.org/2000/svg'>
-				<g clip-path='url(#clip0_331_2)'>
+			<div
+				style={{
+					width: '100%',
+					height: '100%',
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'center'
+				}}>
+				<svg
+					width='90'
+					height='90'
+					viewBox='0 0 90 90'
+					fill='none'
+					xmlns='http://www.w3.org/2000/svg'>
 					<rect
-						width='30'
-						height='30'
+						width='90'
+						height='90'
 						fill='white'
 					/>
 					<rect
-						x='10'
-						y='16.6667'
-						width='3.33333'
-						height='3.33333'
+						x='45'
+						y='45'
+						width='10'
+						height='10'
+						fill='white'
+					/>
+					<rect
+						x='30'
+						y='50'
+						width='10'
+						height='10'
 						fill='black'
 					/>
 					<rect
-						x='10'
-						y='13.3333'
-						width='3.33333'
-						height='3.33333'
+						x='30'
+						y='40'
+						width='10'
+						height='10'
 						fill='black'
 					/>
 					<rect
-						x='10'
-						y='10'
-						width='3.33333'
-						height='3.33333'
+						x='30'
+						y='30'
+						width='10'
+						height='10'
 						fill='black'
 					/>
 					<rect
-						x='13.3334'
-						y='13.3333'
-						width='3.33333'
-						height='3.33333'
+						x='40'
+						y='40'
+						width='10'
+						height='10'
 						fill='black'
 					/>
 					<rect
-						x='16.6666'
-						y='10'
-						width='3.33333'
-						height='3.33333'
+						x='50'
+						y='30'
+						width='10'
+						height='10'
 						fill='black'
 					/>
-				</g>
-				<defs>
-					<clipPath id='clip0_331_2'>
-						<rect
-							width='30'
-							height='30'
-							fill='white'
-						/>
-					</clipPath>
-				</defs>
-			</svg>
-		)
+				</svg>
+			</div>
+		),
+		{...size}
 	)
 }
