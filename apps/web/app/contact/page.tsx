ea07d99899e4a4d21f3abc82213dfc5b30653770
Sarim Malik <sarim@rubriclab.com>
2023-10-15 01:53:59 -0400

    Add contact page

--- apps/web/app/contact/page.tsx
+++ apps/web/app/contact/page.tsx
@@ -1,7 +1,58 @@
+'use client'
+
+import {toast} from '@rubriclab/ui'
+import {useState} from 'react'
+import {copyToClipboard, sendSlackMessage} from '../../lib/utils'
+import Button from '../components/Button'
+
 export default function Contact() {
+	const [sending, setSending] = useState(false)
+	const [name, setName] = useState('')
+	const [email, setEmail] = useState('')
+	const [message, setMessage] = useState('')
+
+	async function handleSubmit() {
+		if (name && email && message)
+			try {
+				setSending(true)
+				await sendSlackMessage(name, email, message)
+			} catch (error) {
+			} finally {
+				setSending(false)
+			}
+		else toast.error('Missing field')
+	}
+
 	return (
-		<div className='flex min-h-screen flex-col items-center justify-center bg-white dark:bg-black'>
-			<h1>Contact</h1>
+		<div className='flex min-h-screen flex-col items-center justify-center p-5 sm:px-10'>
+			<h1>Get in touch</h1>
+			<div className='flex w-full max-w-sm flex-col gap-5'>
+				<input
+					placeholder='Name'
+					onChange={e => setName(e.target.value)}
+				/>
+				<input
+					placeholder='Email'
+					onChange={e => setEmail(e.target.value)}
+				/>
+				<textarea
+					placeholder='Message'
+					onChange={e => setMessage(e.target.value)}
+				/>
+				<Button
+					body='Submit'
+					variant='dark'
+					onClick={() => handleSubmit()}
+				/>
+				<span className='text-sm'>
+					Alternatively, feel free to email us at{' '}
+					<button
+						className='font-semibold underline-offset-4 hover:underline'
+						onClick={() => copyToClipboard('hello@rubriclab.com')}>
+						hello@rubriclab.com
+					</button>
+				</span>
+			</div>
 		</div>
 	)
 }
--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -1,5 +1,8 @@
+import {toast} from '@rubriclab/ui'
+import axios, {AxiosPromise, Method} from 'axios'
 import clsx, {type ClassValue} from 'clsx'
 import {Metadata} from 'next'
+import qs from 'qs'
 import {twMerge} from 'tailwind-merge'
 import {DEFAULT_META, META} from './constants'
 
@@ -55,4 +58,60 @@ export const getMetadata = ({
 // Allows merging of Tailwind class
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
+}
+
+// Copy to clipboard
+export function copyToClipboard(input: string) {
+	navigator.clipboard.writeText(input)
+	toast.success('Email copied')
+}
+
+// Send slack message
+export async function sendSlackMessage(
+	name: string,
+	email: string,
+	message: string
+) {
+	const url = 'https://slack.com/api/chat.postMessage'
+
+	const payload = {
+		token: process.env.SLACK_KEY,
+		channel: 'C05AQKAJL9X', // #sales channel ID
+		text: `${name}\n${email}\n${message}\n`
+	}
+
+	await apiRequest(url, 'POST', qs.stringify(payload)).catch((error: Error) => {
+		console.error(error)
+	})
+}
+
+/**
+ * Send an API request with headers that accepts JSON response
+ */
+export function apiRequest(
+	url: string,
+	method: Method,
+	data = {},
+	headers = {}
+): AxiosPromise {
+	// Ensure internet is connected
+	if (!(window && window.navigator && window.navigator.onLine))
+		return Promise.reject('Disconnected from internet')
+
+	// Accept JSON responses and do not cache response
+	const defaultHeaders = {
+		Accept: 'application/json',
+		'Content-Type': 'application/json'
+	}
+
+	// Combine headers
+	const combinedHeaders = {...defaultHeaders, ...headers}
+
+	// Clear then set headers in Axios
+	axios.defaults.headers.common = {}
+	Object.keys(combinedHeaders).map(header => {
+		axios.defaults.headers.common[header] = combinedHeaders[header]
+	})
+
+	return axios({method, url, data})
 }
