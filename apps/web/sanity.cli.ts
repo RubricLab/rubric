6ee897b04e927790ce25ac5b96f473a55a5eca8a
Sarim Malik <sarim@rubriclab.com>
2023-10-17 20:05:33 -0400

    Migrate website to new repository

--- apps/web/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import {defineCliConfig} from 'sanity/cli'
-
-export default defineCliConfig({
-	api: {
-		dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-		projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT
-	}
-})
--- packages/@rubriclab/tailwind-config/tailwind.ts
+++ packages/@rubriclab/tailwind-config/tailwind.ts
@@ -3,7 +3,10 @@ import _colors from 'tailwindcss/colors'
 
 type Optional<T, K extends keyof T> = Omit<T, K> & Partial<T>
 
-type Colors = Optional<typeof _colors, 'lightBlue' | 'warmGray' | 'trueGray' | 'coolGray' | 'blueGray'>
+type Colors = Optional<
+	typeof _colors,
+	'lightBlue' | 'warmGray' | 'trueGray' | 'coolGray' | 'blueGray'
+>
 
 const colors = _colors as Colors
 
