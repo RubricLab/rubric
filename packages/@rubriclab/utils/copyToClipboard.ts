63d813fa2b917b1588e61683f5145c47176c2a30
DexterStorey <dexter@dexterstorey.com>
2024-03-09 13:57:01 -0500

    tweaks to exports

--- packages/@rubriclab/utils/copyToClipboard.ts
+++ packages/@rubriclab/utils/copyToClipboard.ts
@@ -2,7 +2,7 @@
  * Copy text to the clipboard.
  * Throws an error if clipboard is unavailable.
  */
-export const copyToClipboard = (
+export default function copyToClipboard(
 	text: string,
 	{
 		onSuccess,
@@ -11,8 +11,9 @@ export const copyToClipboard = (
 		onSuccess?: (res?: string | void) => void
 		onError?: (error?: Error) => void
 	} = {}
-) => {
-	if (!window?.navigator) throw new Error('Please try copying the text manually. Something went wrong.')
+) {
+	if (!window?.navigator)
+		throw new Error('Please try copying the text manually. Something went wrong.')
 
 	navigator?.clipboard
 		?.writeText(text)
