fd50e505af78e630614d30ac98ecb2c5b504a4d3
DexterStorey <dexter@dexterstorey.com>
2025-07-17 22:17:51 -0400

    format

--- scripts/github.ts
+++ scripts/github.ts
@@ -5,33 +5,33 @@ import { $ } from 'bun'
 import { z } from 'zod'
 
 createCLI({
-	name: 'github',
-	version: '1.0.0',
-	description: 'Submodule management for Rubric',
 	commands: [
 		{
-			name: 'add-app',
-			description: 'Adds a submodule to the repository',
 			args: z.object({
 				name: z.string().describe('Name of the submodule'),
 				repo: z.string().describe('URL of the submodule')
 			}),
+			description: 'Adds a submodule to the repository',
 			handler: async ({ name, repo }) => {
 				console.log(`Adding ${name} from ${repo}`)
 				await $`git submodule add ${repo} apps/${name}`
-			}
+			},
+			name: 'add-app'
 		},
 		{
-			name: 'add-package',
-			description: 'Adds a submodule to the repository',
 			args: z.object({
 				name: z.string().describe('Name of the submodule'),
 				repo: z.string().describe('URL of the submodule')
 			}),
+			description: 'Adds a submodule to the repository',
 			handler: async ({ name, repo }) => {
 				console.log(`Adding ${name} from ${repo}`)
 				await $`git submodule add ${repo} packages/${name}`
-			}
+			},
+			name: 'add-package'
 		}
-	]
+	],
+	description: 'Submodule management for Rubric',
+	name: 'github',
+	version: '1.0.0'
 }).parse()
